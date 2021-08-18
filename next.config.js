75636b22704977452f03c9394b51fb86e7e87391
Olga F. Andreiko <61794263+olga-f@users.noreply.github.com>
2021-08-18 16:50:43 +0200

    add next config and env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  reactStrictMode: true,
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+
+      // React's Strict Mode is a development mode only feature for highlighting potential problems in an application
+      reactStrictMode: true,
+    };
+  }
+
+  return {
+    /* config options for all phases except development here */
+  };
 };
