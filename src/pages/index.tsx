7abe7ff3feaf0a096f091008c6c81808fd092a1a
emege93 <emege93@gmail.com>
2021-12-01 15:35:19 +0100

    add config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import React from "react";
-import Main from "../components/main";
+import Main from "@/components/main";
 export default function Home() {
   return (
     <>
